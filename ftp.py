a9860a261f283381368398849ad5203219288e5e
Rodrigo Benevides <rbenevid@gmail.com>
2017-01-15 03:03:53 -0200

    Usando MLSD pra retornar os dados do FTP Coloquei um main de teste

--- ftp.py
+++ ftp.py
@@ -33,9 +33,47 @@ class Ftp(object):
         if self.ftp is None:
             return []
         else:
-            #self.ftp.retrlines('MLSD')
             resultado = []
-            self.ftp.retrlines('LIST', resultado.append)
-            return resultado
+            retorno = []
+            self.ftp.retrlines('MLSD', resultado.append)
+            for cada_linha in resultado:
+                entrada = EntradaFtp(cada_linha)
+                if entrada.is_file():
+                    retorno.append(entrada)
+            return retorno
 
 
+class EntradaFtp(object):
+    """ Representa um arquivo no ftp """
+    tipo = None
+    data = None
+    nome = None
+
+    def __init__(self, linha_mlsd):
+        for item in linha_mlsd.split(';'):
+            atribuicao = item.partition('=')
+            if atribuicao[1] == '=':
+                campo = atribuicao[0]
+                if campo == 'type':
+                    self.tipo = atribuicao[2]
+                elif campo == 'modify':
+                    self.data = atribuicao[2]
+            else:
+                self.nome = item.strip()
+
+    def is_file(self):
+        """ Retorna true se for arquivo """
+        return self.tipo == 'file' or self.tipo is None
+
+    def to_string(self):
+        """ Retorna uma descrição amigável do item """
+        return '%s - %s - %s' % (self.tipo, self.data, self.nome)
+
+if __name__ == "__main__":
+    """ Como teste, lista o conteúdo de um ftp da internet """
+    ftp = Ftp('ftp.funet.fi', 'anonymous', 'senha_qualquer')
+    ftp.conecta()
+    ftp.muda_diretorio('/pub/msx')
+    for cada in ftp.lista_arquivos():
+        print cada.to_string()
+    ftp.desconecta()
