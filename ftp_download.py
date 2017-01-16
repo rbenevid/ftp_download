a6d085a5db794aac640436986e83b092f45da5ab
Rodrigo Benevides <rodrigo@mpf.mp.br>
2017-01-16 18:09:13 -0200

    - Detalhei melhor o log - Mostro antes de fazer o download tudo que vai fazer (qtde e tamanho total de arquivos) -  Os arquivos do FTP são ordenados antes de processar

--- ftp_download.py
+++ ftp_download.py
@@ -11,11 +11,12 @@ import sys
 import logging
 from configuracao import Configuracao
 from repositorio import Repositorio
-from ftp import Ftp
+from ftp import Ftp, convert_bytes
+
 
 def main():
     """ Dispara o programa """
-    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.DEBUG)
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s', level=logging.INFO)
 
     if len(sys.argv) < 2:
         logging.error('Passe o arquivo de configuração JSON como argumento, por favor')
@@ -32,17 +33,38 @@ def main():
     ftp.muda_diretorio(config.diretorio)
     arquivos = ftp.lista_arquivos()
     logging.info('Arquivos no FTP: %s', len(arquivos))
+    arquivos.sort(key=lambda arq: -arq.tamanho)
     baixados = 0
     ignorados = 0
+    tamanho_total = 0
+    pendencias = []
     for arquivo in arquivos:
+        ignora = False
+        tamanho_local = repositorio.get_tamanho(arquivo.nome)
+        #print 'Tamanho repos %s, tamanho FTP %s' % (tamanho_local, arquivo.tamanho)
+        msg = None
         if not repositorio.existe(arquivo.nome):
-            logging.info('Baixando %s (%s bytes)', arquivo.nome, arquivo.tamanho)
+            msg = 'Não existe - baixando %s (%s)'
+        elif tamanho_local != arquivo.tamanho:
+            msg = 'Tamanho diferente - baixando %s (%s)'
+        else:
+            ignora = True
+        if not ignora:
+            pendencias.append({'nome': arquivo.nome, 'tamanho': arquivo.tamanho, 'msg': msg})
+            tamanho_total += arquivo.tamanho
             baixados += 1
         else:
-            logging.debug('Arquivo já existe: %s', arquivo.nome)
+            #logging.debug('Arquivo já existe: %s', arquivo.nome)
             ignorados += 1
+    if len(pendencias) > 0:
+        logging.info('Arquivos a baixar: %s (%s)', len(pendencias), convert_bytes(tamanho_total))
+        for pendencia in pendencias:
+            logging.info(pendencia['msg'], pendencia['nome'], convert_bytes(pendencia['tamanho']))
+            ftp.download(pendencia['nome'], pendencia['tamanho'], config.repositorio)
+        logging.info('Fim do processamento, %s baixados, %s ignorados', baixados, ignorados)
+    else:
+        logging.info('Todos os arquivos já existem, nada a fazer')
     ftp.desconecta()
-    logging.info('Fim do processamento, %s baixados, %s ignorados', baixados, ignorados)
 
 
 if __name__ == '__main__':
